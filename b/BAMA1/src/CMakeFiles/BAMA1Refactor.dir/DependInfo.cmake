88093e994c8ee1b98f264271dc0a1700ef70b3bf
jsmartini <jsmartini@crimson.ua.edu>
2022-11-23 00:10:53 -0600

    Successfully built BAMA1Refactor Binary with test from libcsp

--- b/BAMA1/src/CMakeFiles/BAMA1Refactor.dir/DependInfo.cmake
+++ b/BAMA1/src/CMakeFiles/BAMA1Refactor.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/jsmartini/Desktop/BAMA-1 C++/BAMA1/src/main.c" "BAMA1/src/CMakeFiles/BAMA1Refactor.dir/main.c.o" "gcc" "BAMA1/src/CMakeFiles/BAMA1Refactor.dir/main.c.o.d"
+  "/home/jsmartini/Desktop/BAMA-1 C++/BAMA1/src/main.cxx" "BAMA1/src/CMakeFiles/BAMA1Refactor.dir/main.cxx.o" "gcc" "BAMA1/src/CMakeFiles/BAMA1Refactor.dir/main.cxx.o.d"
   )
 
 # Targets to which this target links.
--- b/BAMA1/src/CMakeFiles/BAMA1Refactor.dir/cmake_clean.cmake
+++ b/BAMA1/src/CMakeFiles/BAMA1Refactor.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
   "BAMA1Refactor"
   "BAMA1Refactor.pdb"
-  "CMakeFiles/BAMA1Refactor.dir/main.c.o"
-  "CMakeFiles/BAMA1Refactor.dir/main.c.o.d"
+  "CMakeFiles/BAMA1Refactor.dir/main.cxx.o"
+  "CMakeFiles/BAMA1Refactor.dir/main.cxx.o.d"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang CXX)
   include(CMakeFiles/BAMA1Refactor.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
